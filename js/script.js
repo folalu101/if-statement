56540bafb1622c951c2dbb44dcc2d829c1c3bf35
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-28 15:51:58 +0100

    create if statement for age

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// example of If-Statement
+
+let age = parseInt(prompt());
+
+if (age ===0){
+    alert('please enter a valid age')
+}
+else if(age <=17){
+    alert('you are still a teenager. Enjoy it while it last')
+}
+else if(age ===18 && )
